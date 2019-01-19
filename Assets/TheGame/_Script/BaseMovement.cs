8719441e53c85cdfc6efd97249caeeff8f8ab702
carl69 <carlmorgenstierne@yahoo.com>
2019-01-19 12:16:57 +0100

    tweking the movement

--- Assets/TheGame/_Script/BaseMovement.cs
+++ Assets/TheGame/_Script/BaseMovement.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BaseMovement : MonoBehaviour {
-    float placeholderMovementSpeed = 10;
-    float placeholderRotateDampening = 10;
+    float placeholderMovementSpeed = 5;
+    float placeholderRotateDampening = 20;
 
     public Transform placeholderTarget;
+
+    public enum PlayerState
+    {
+        Walking,Casting,Attacking
+    }
+    public PlayerState currentPlayerState;
+
 
 	// Use this for initialization
 	void Start () {
@@ -15,18 +22,33 @@ public class BaseMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        // very basic looking
 
-        var lookPos = placeholderTarget.position - transform.position;
-        lookPos.y = 0;
-        var rotation = Quaternion.LookRotation(lookPos);
-        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * placeholderRotateDampening);
+        switch (currentPlayerState)
+        {
+            case PlayerState.Walking:
+                // very basic looking
 
-        // very basic walking
-        
-        Vector3 RIGHT = transform.TransformDirection(Vector3.right);
-        Vector3 FORWARD = transform.TransformDirection(Vector3.forward);
-        transform.localPosition += RIGHT * Input.GetAxis("Horizontal") * placeholderMovementSpeed * Time.deltaTime;
-        transform.localPosition += FORWARD * Input.GetAxis("Vertical") * placeholderMovementSpeed * Time.deltaTime;
+                var lookPos = placeholderTarget.position - transform.position;
+                lookPos.y = 0;
+                var rotation = Quaternion.LookRotation(lookPos);
+                transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * placeholderRotateDampening);
+
+                // very basic walking
+
+                Vector3 RIGHT = transform.TransformDirection(Vector3.right);
+                Vector3 FORWARD = transform.TransformDirection(Vector3.forward);
+                transform.localPosition += RIGHT * Input.GetAxis("Horizontal") * placeholderMovementSpeed / 1.5f * Time.deltaTime;
+                transform.localPosition += FORWARD * Input.GetAxis("Vertical") * placeholderMovementSpeed * Time.deltaTime;
+
+
+                break;
+            case PlayerState.Casting:
+                break;
+            case PlayerState.Attacking:
+                break;
+            default:
+                break;
+        }
+
     }
 }
