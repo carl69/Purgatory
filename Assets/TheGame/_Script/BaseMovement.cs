18a0f5aaaca24687ae1598f01946391375617a6b
carl69 <carlmorgenstierne@yahoo.com>
2019-01-19 11:57:26 +0100

    very basic movement

--- Assets/TheGame/_Script/BaseMovement.cs
+++ Assets/TheGame/_Script/BaseMovement.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BaseMovement : MonoBehaviour {
+    float placeholderMovementSpeed = 10;
+    float placeholderRotateDampening = 10;
+
+    public Transform placeholderTarget;
 
 	// Use this for initialization
 	void Start () {
@@ -10,7 +14,19 @@ public class BaseMovement : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void FixedUpdate () {
+        // very basic looking
+
+        var lookPos = placeholderTarget.position - transform.position;
+        lookPos.y = 0;
+        var rotation = Quaternion.LookRotation(lookPos);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * placeholderRotateDampening);
+
+        // very basic walking
+        
+        Vector3 RIGHT = transform.TransformDirection(Vector3.right);
+        Vector3 FORWARD = transform.TransformDirection(Vector3.forward);
+        transform.localPosition += RIGHT * Input.GetAxis("Horizontal") * placeholderMovementSpeed * Time.deltaTime;
+        transform.localPosition += FORWARD * Input.GetAxis("Vertical") * placeholderMovementSpeed * Time.deltaTime;
+    }
 }
