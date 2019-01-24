2d560ae885838d13a7e31c6b0d6455ca48ddbcfb
Pablo <pablma11@ucm.es>
2019-01-24 12:12:20 +0100

    State Machine base class built

---
+++ Unity/Assets/TheGame/_Script/Player/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Current state of the player
+    private State currentState;
+
+    private void Start()
+    {
+        //SetState(new BasicMovementState(this));
+    }
+
+    private void Update()
+    {
+        currentState.Tick();
+    }
+
+    // Method to set a new state
+    public void SetState(State state)
+    {
+        if (currentState != null)
+            currentState.OnStateExit();
+
+        currentState = state;
+
+        if (currentState != null)
+            currentState.OnStateEnter();
+    }
+}
---
+++ Unity/Assets/TheGame/_Script/StateMachine/State.cs
@@ -0,0 +1,19 @@
+﻿// Base class for the states
+public abstract class State
+{
+    // A reference for the player
+    protected Player player;
+
+    // Abstract method, child classes are forced to implement it
+    public abstract void Tick();
+
+    // Virtual methods, child classes are not obligated to implement them
+    public virtual void OnStateEnter() { }
+    public virtual void OnStateExit() { }
+
+    // Constructor of the base class
+    public State(Player p)
+    {
+        this.player = p;
+    }
+}
