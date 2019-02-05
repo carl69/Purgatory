96b4bd1ab381d552f290943e6460d991d2bc05b2
rebo95 <arebored@ucm.es>
2019-02-05 13:41:21 +0100

    start dodge with raycast logic

--- Unity/Assets/TheGame/_Script/Player/PlayerStates/DashState.cs
+++ Unity/Assets/TheGame/_Script/Player/PlayerStates/DashState.cs
@@ -8,6 +8,7 @@ public class DashState : State
     Vector3 newPosition;
     Vector3 dashDirection_;
     float dashDistance_ = 3;
+
     public float smoothLevel = 5;
 
     public DashState(Player player, Vector3 dashDirection) : base(player){
@@ -18,6 +19,12 @@ public class DashState : State
 
     public override void Tick()
     {
+        ///////////////////////////////////
+        Vector3 raycastOrigin = new Vector3(player.transform.position.x, player.transform.position.y /*- player.transform.localScale.y*/, player.transform.position.z);
+        Ray playerRay = new Ray(raycastOrigin, dashDirection_);
+        Debug.DrawRay(raycastOrigin, dashDirection_, Color.red, dashDistance_);
+        //////////////////////////////////
+
         player.transform.position = Vector3.Lerp(player.transform.position, newPosition, Time.deltaTime * smoothLevel);
         if (player.transform.position.x > newPosition.x - 0.2 && player.transform.position.x < newPosition.x + 0.2 && player.transform.position.z > newPosition.z - 0.2 && player.transform.position.z < newPosition.z + 0.2)
         {
@@ -31,5 +38,20 @@ public class DashState : State
         newPosition = player.transform.position;
         newPosition = player.transform.position + dashDirection_.normalized * dashDistance_;
         //player.SetState(new MovementState(player));
+
+        //method that calculates if the new position is accessible.
+        checkNewPosition();
+
+    }
+    void checkNewPosition()
+    {
+        RaycastHit hit;
+        Vector3 raycastOrigin = new Vector3(player.transform.position.x, player.transform.position.y /*- player.transform.localScale.y +3f*/, player.transform.position.z);
+        Ray playerRay = new Ray(raycastOrigin, dashDirection_);
+
+        if(Physics.Raycast(playerRay, out hit, dashDistance_))
+        {
+            dashDistance_ = hit.distance;
+        }
     }
 }
--- Unity/Assets/TheGame/_Script/Player/PlayerStates/MovementState.cs
+++ Unity/Assets/TheGame/_Script/Player/PlayerStates/MovementState.cs
@@ -20,8 +20,9 @@ public class MovementState : State
 
     public override void Tick()//MovementStatement update
     {
+        //horizontal plane movement (3D)
         movementDirection = (player.transform.forward * Input.GetAxis("Vertical") * moveSpeed) + (player.transform.right * Input.GetAxis("Horizontal") * moveSpeed);
-
+        //vertical movement(3D) 
         movementDirection.y = movementDirection.y + gravityScale * (Physics.gravity.y * Time.deltaTime);
 
         // GONNA PUT PLACEHOLDER CODE HERE
@@ -43,14 +44,24 @@ public class MovementState : State
             player.transform.GetChild(0).GetComponent<Animator>().SetTrigger("Attack");
 
         }
-        if (Input.GetButtonDown("Dash"))
+
+
+        if (Input.GetButtonDown("Dash"))//when the dash button is pressed we make the calculations of movement direction and use this information that it is going to be used for the DashState
         {
-            Vector3 forwardMovement = player.transform.forward * Input.GetAxis("Vertical");
-            Vector3 sideMovement = player.transform.right * Input.GetAxis("Horizontal");
+            Vector3 dahsDirection;
+            if (movementDirection.x == 0 && movementDirection.z == 0)
+            {//if the player is not movig the dash is backwards
+                dahsDirection =  -player.transform.forward;
+                player.SetState(new DashState(player, dahsDirection));
+            }
+            else
+            {//if not we calculate the dash direction on the direction of the player movement
+                Vector3 forwardMovement = player.transform.forward * Input.GetAxis("Vertical");
+                Vector3 sideMovement = player.transform.right * Input.GetAxis("Horizontal");
+                dahsDirection = sideMovement + forwardMovement;
 
-            Vector3 dahsDirection = sideMovement + forwardMovement;
-
-            player.SetState(new DashState(player, dahsDirection));
+                player.SetState(new DashState(player, dahsDirection));
+            }
         }
 
         controller.Move(movementDirection * Time.deltaTime);
