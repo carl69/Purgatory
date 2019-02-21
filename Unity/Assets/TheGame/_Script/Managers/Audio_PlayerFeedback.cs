47a9fe04190e8c5947116b25eda9a60a2e4c52c0
dotnormalized <31924027+dotnormalized@users.noreply.github.com>
2019-02-21 11:52:12 +0100

    Added scripts, prefabs, managers, sprites, audio from prototype

---
+++ Unity/Assets/TheGame/_Script/Managers/Audio_PlayerFeedback.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audio_PlayerFeedback : MonoBehaviour
+{
+
+    public AudioClip LungeAttack;
+    public AudioClip SloppyStab;
+    public AudioClip FerventAnger;
+    public AudioClip SureStrike;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    //Attack-sounds
+
+    //Lunge Attack
+    public void LungeSFX()
+    {
+        gameObject.transform.GetChild(3).transform.GetChild(0).GetComponent<AudioSource>().PlayOneShot(LungeAttack);
+    }
+    // Sloppy Stab
+    public void SloppySFX()
+    {
+        gameObject.transform.GetChild(3).transform.GetChild(0).GetComponent<AudioSource>().PlayOneShot(SloppyStab);
+    }
+
+    // Fervent Anger
+    public void FerventSFX()
+    {
+        gameObject.transform.GetChild(3).transform.GetChild(0).GetComponent<AudioSource>().PlayOneShot(FerventAnger);
+    }
+
+    // Sure Strike
+    public void SureStrikeSFX()
+    {
+        gameObject.transform.GetChild(3).transform.GetChild(0).GetComponent<AudioSource>().PlayOneShot(SureStrike);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+
+        }
+    }
+}
---
+++ Unity/Assets/TheGame/_Script/MenusManager/MenuBehaviour.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class MenuBehaviour : MonoBehaviour
+{
+    public GameObject AudioManagerObject;
+    private GameObject PauseMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioManagerObject = GameObject.FindGameObjectWithTag("AudioManager");
+        
+        if(GameObject.FindGameObjectWithTag("Menu_Pause").gameObject != null)
+        {
+            PauseMenu = GameObject.FindGameObjectWithTag("Menu_Pause").gameObject;
+            PauseMenu.SetActive(false);
+        }
+    
+    }
+    
+    //Button functions
+    public void NewGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void NextScreen()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void GoToArena()
+    {
+        SceneManager.LoadScene(3);
+        AudioManagerObject.GetComponent<AudioManager>().ArenaStart();
+    }
+
+    public void ReturnToMain()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Resume()
+    {
+        PauseMenu.SetActive(false);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void PlaySFX()
+    {
+        AudioManagerObject.GetComponent<AudioManager>().MenuClickSFX();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            PauseMenu.SetActive(true);
+        }
+    }
+}
