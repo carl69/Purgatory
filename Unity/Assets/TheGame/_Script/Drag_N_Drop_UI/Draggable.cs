9f1d95e987bb2498420cd83411b0a2f0b88c7c78
Pablo <pablma11@ucm.es>
2019-03-02 17:39:42 +0100

    Drag N Drop Menu enhanced

--- Unity/Assets/TheGame/_Script/Drag_N_Drop_UI/Draggable.cs
+++ Unity/Assets/TheGame/_Script/Drag_N_Drop_UI/Draggable.cs
@@ -6,15 +6,37 @@ using UnityEngine.EventSystems;
 public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
 
+    // A variable to store the parent which the card must return if it isn't dropped in a correct place
+    private Transform parentToReturnTo;
+    public Transform ParentToReturnTo { get { return this.parentToReturnTo; } set { this.parentToReturnTo = value; } }
+
+    // A variable to unlock and lock the raycasts when dragging and object
+    private CanvasGroup canvasGroup;
+
     // Variable to store the offset when we click an object form every point
     private Vector2 offset;
 
 
+    private void Start()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
     // Called when the object starts to being dragged
     public void OnBeginDrag(PointerEventData eventData)
     {
+        // Calculations
         offset.x = this.transform.position.x - eventData.position.x;
         offset.y = this.transform.position.y - eventData.position.y;
+
+        // We update the parent which the object must return to
+        parentToReturnTo = this.transform.parent;
+
+        // We change the parent of the object
+        this.transform.SetParent(this.transform.parent.parent);
+
+        // We set the raycast free
+        canvasGroup.blocksRaycasts = false;
     }
 
     // Called when the object is being dragged
@@ -26,6 +48,9 @@ public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDra
     // Called when the object finishes of being dragged
     public void OnEndDrag(PointerEventData eventData)
     {
+        this.transform.SetParent(parentToReturnTo);
 
+        // We block the raycast
+        canvasGroup.blocksRaycasts = true;
     }
 }
---
+++ Unity/Assets/TheGame/_Script/Drag_N_Drop_UI/DropZone.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler
+{
+
+    // Variable to get access to the object dragged and change its parent
+    Draggable d;
+
+    // Called when the pointer of the mouse enters
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+    }
+
+    // Called when the pointer of the mouse leaves
+    public void OnPointerExit(PointerEventData eventData)
+    {
+    }
+
+    // Called when something is dropped
+    public void OnDrop(PointerEventData eventData)
+    {
+        d = eventData.pointerDrag.GetComponent<Draggable>();
+
+        if (d != null)
+            d.ParentToReturnTo = this.transform;
+    }
+}
