0b7a7cec23c896298c63610fd74ce8c33e485994
rebo95 <arebored@ucm.es>
2019-03-05 14:15:31 +0100

    healthBar added, draft logic added

---
+++ Unity/Assets/TheGame/_Script/Player/PlayerHealth/FaceCamera.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceCamera : MonoBehaviour
+{
+
+    public Camera enemyPlayerCamera;
+
+    private void LateUpdate()
+    {
+        transform.LookAt(enemyPlayerCamera.transform);
+    }
+}
---
+++ Unity/Assets/TheGame/_Script/Player/PlayerHealth/Health.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+
+    public RectTransform healthBar;
+
+    public int maxHealth = 100;
+    int damageReceived = 10;
+    int health;
+
+    public bool damaged = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (damaged)
+        {
+            takeDamage();
+            damaged = !damaged;
+        }
+    }
+
+    public void takeDamage()
+    {
+        health -= damageReceived;
+
+        healthBar.sizeDelta = new Vector2(health * 2, healthBar.sizeDelta.y);
+    }
+}
