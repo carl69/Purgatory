bfad98b53244c68ee7af81540b277412b709099e
rebo95 <arebored@ucm.es>
2019-03-05 12:03:42 +0100

    camera target position now editable from editor

---
+++ Unity/Assets/TheGame/_Script/Camera/CameraTargetEditor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTargetEditor : MonoBehaviour
+{
+    [SerializeField]
+    private Transform cameraT;
+
+    [SerializeField]
+    [Range(-10.0f, 10.0f)]
+    private float cameraTHeight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraTHeight = cameraT.localPosition.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cameraT.localPosition = new Vector3(cameraT.localPosition.x, cameraTHeight, cameraT.localPosition.z);
+    }
+}
